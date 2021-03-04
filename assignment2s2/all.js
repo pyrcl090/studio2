e96f99904731bf2342aafaa3ec970a96f4c649a9
pyrcl090 <70988666+pyrcl090@users.noreply.github.com>
2021-03-04 09:30:08 -0500

    Add files via upload

---
+++ assignment2s2/all.js
@@ -0,0 +1,22 @@
+
+// window.onscroll = function() {myFunction()};
+
+// var large = document.getElementById("large");
+
+// var fixed = large.offsetTop;
+
+// function myFunction() {
+//     if (window.pageYOffset >= fixed) 
+//     {
+//     large.classList.add("fixed")
+//     } 
+//     else 
+//     {
+//     large.classList.remove("fixed");
+//     }
+// }
+// const cursor = document.querySelector('.cursor');
+// document.addEventListener('mousemove', (e) => {
+//     cursor.style.left = e.pageX + 'px';
+//     cursor.style.top = e.pageY + 'px';
+
